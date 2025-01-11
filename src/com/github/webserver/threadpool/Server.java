430239a962ba48a4044a5c09e7544ce68649678b
Ayush Rawat <ayushrawat3338@gmail.com>
2025-01-11 21:19:45 +0530

    thread pool init

--- src/com/github/webserver/threadpool/Server.java
+++ src/com/github/webserver/threadpool/Server.java
@@ -1,4 +1,46 @@
 package com.github.webserver.threadpool;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
 public class Server {
+
+  private static final Logger logger = Logger.getLogger(Server.class.getName());
+  private static final int PORT = 8011, POOL_SIZE = 10;
+  private final ExecutorService threadPool; /// shout out to Author: Doug Lea
+
+  public Server(int poolSize) {
+    this.threadPool = Executors.newFixedThreadPool(poolSize);
+  }
+
+  public void handleClient(Socket clientSocket) {
+    try (PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(), true)) {
+      toSocket.println("Hello from server " + clientSocket.getInetAddress());
+    } catch (IOException ex) {
+      logger.severe(ex.getMessage());
+    }
+  }
+
+  public static void main(String[] args) {
+    Server server = new Server(POOL_SIZE);
+
+    try(ServerSocket serverSocket = new ServerSocket(PORT)) {
+      serverSocket.setSoTimeout(70000);
+      logger.info("Server is listening on port " + PORT);
+
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        server.threadPool.execute(() -> server.handleClient(clientSocket));
+      }
+    } catch (IOException ex) {
+      logger.severe(ex.getMessage());
+    } finally {
+      server.threadPool.shutdown();
+    }
+  }
 }
